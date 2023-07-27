80751f0bad8ac58d3c0da2689e391bf59d503055
Jadriel Delim <jadrieldelim@gmail.com>
2023-07-27 09:51:53 -0400

    Deleted SampleScene, started WinCount

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,26 @@
 using BoardGames;
 using UnityEngine;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
     public TicTacToe TicTacToeGame;
+    public TextMeshPro P1WinCount;
+    public TextMeshPro P2WinCount;
+    private int P1Wins, P2Wins;
     public void GetInput (int col, int row)
     {
         TicTacToeGame.Move(col, row);
+        if (TicTacToeGame.CurrentState == GameState.Player1Win)
+        {
+            P1Wins++;
+        }
+        if (TicTacToeGame.CurrentState == GameState.Player2Win)
+        {
+            P2Wins++;
+        }
+        P1WinCount.text = "Player 1 Win Count:\n" + P1Wins.ToString();
+        P2WinCount.text = "Player 2 Win Count:\n" + P2Wins.ToString();
     }
     // Start is called before the first frame update
     void Start()
